3e2614f3abb68711e977d293a622b455fc0c5555
Андрей <anderkoval3@gmail.com>
2019-06-24 21:09:04 +0300

    some small steps of creating simple things =)

--- test.js
+++ test.js
@@ -1,26 +1,8 @@
-// var a = eval("console.log(10 - 5 + 4*4)") // wtf))
-// // console.log(a)
-// a = 10/1
-// if (a == Infinity) {
-//     console.log('Division by 0!!!')
-// } else
-//     console.log(a)
-//     console.log(a + " repeat") //wtf again))
-
-// function testFunc(a, b, c) {
-    //     console.log(a + " " + b + ' ' + c)
-    //     console.log(c.c)
-    //     return false;
-    // }
-    
-    
-var user = prompt("Please enter your name")
-if (user === null) {
-    alert("LOX")
-} else { 
-    if (confirm("Do you really want to give us your name??"))
-        alert("Заебок")
-    else 
-        alert("Molodec")
+function printDate() {
+  var curr_date = new Date()
+  let elem = document.getElementById("demo")
+  elem.innerHTML = curr_date.toUTCString()
+  console.log(elem)
 }
-    
+
+setInterval(printDate, 999)
